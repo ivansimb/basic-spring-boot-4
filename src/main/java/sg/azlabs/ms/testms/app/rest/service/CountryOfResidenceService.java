b18ddae878faf4d2d63d7ce91ccf9804500c82f0
ivansimb <ivan@azlabs.com>
2022-05-27 16:28:22 +0700

    test mvn profile and location ECUADOR

--- src/main/java/sg/azlabs/ms/testms/app/rest/service/CountryOfResidenceService.java
+++ src/main/java/sg/azlabs/ms/testms/app/rest/service/CountryOfResidenceService.java
@@ -43,7 +43,7 @@ public class CountryOfResidenceService extends BaseService{
         }
 
         CountryOfResidence cor = new CountryOfResidence();
-        cor.setCor("CROATIA");
+        cor.setCor("ECUADOR");
         cor.setUid("1");
         cor.setLanguageCode("EN");
         cor.setPublishFlag(true);
