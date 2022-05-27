031ccf86d2fc1b9910dd72c1001410e3294c3c7c
ivansimb <ivan@azlabs.com>
2022-05-27 09:42:37 +0700

    update to china

--- src/main/java/sg/azlabs/ms/testms/app/rest/service/CountryOfResidenceService.java
+++ src/main/java/sg/azlabs/ms/testms/app/rest/service/CountryOfResidenceService.java
@@ -43,7 +43,7 @@ public class CountryOfResidenceService extends BaseService{
         }
 
         CountryOfResidence cor = new CountryOfResidence();
-        cor.setCor("KOREA");
+        cor.setCor("CHINA");
         cor.setUid("1");
         cor.setLanguageCode("EN");
         cor.setPublishFlag(true);
