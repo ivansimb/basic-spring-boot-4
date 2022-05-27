13f7dd8a6bc968f2eb4e2bd41e17ae7ad74b5eb7
ivansimb <ivan@azlabs.com>
2022-05-27 09:36:29 +0700

    update location test to KOREA

--- src/main/java/sg/azlabs/ms/testms/app/rest/service/CountryOfResidenceService.java
+++ src/main/java/sg/azlabs/ms/testms/app/rest/service/CountryOfResidenceService.java
@@ -43,7 +43,7 @@ public class CountryOfResidenceService extends BaseService{
         }
 
         CountryOfResidence cor = new CountryOfResidence();
-        cor.setCor("BELGIUM");
+        cor.setCor("KOREA");
         cor.setUid("1");
         cor.setLanguageCode("EN");
         cor.setPublishFlag(true);
